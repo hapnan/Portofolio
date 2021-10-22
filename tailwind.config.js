636a00689d916a910ae650fd7d2b8eeefa57eef1
hapnan <hapnanarsad@gmail.com>
2021-10-22 23:46:58 +0700

    page-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'coding': "url('/img/working-code.jpg')",
+        
+       }
+    },
   },
   variants: {
     extend: {},
